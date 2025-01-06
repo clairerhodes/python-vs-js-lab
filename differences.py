7a5fbcb0ce5d0ef6404045aac03366b8e07e8ec2
Claire Rhodes <claire.rhodes13@gmail.com>
2025-01-05 17:30:47 -0800

    hw setup

---
+++ differences.py
@@ -0,0 +1,16 @@
+# Convert the following to Python from JavaScript:
+# ================================ Problem 1 ================================
+# const getName = () => {
+#   let name = prompt("what is your name?");
+#   return name;
+# };
+
+def getName():
+    user_input = input("what is your name?")
+    print("Hello, " + user_input + "!")
+    
+getName()
+
+# ================================ Problem 2 ================================
+
+
