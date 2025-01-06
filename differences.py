fd81c61d21244d8183fe47aa4cf49e64735e0afa
Claire Rhodes <claire.rhodes13@gmail.com>
2025-01-05 17:58:05 -0800

    problem 1

--- differences.py
+++ differences.py
@@ -1,5 +1,9 @@
-# Convert the following to Python from JavaScript:
+# Convert the following to Python from JavaScript
+
+
 # ================================ Problem 1 ================================
+# Write a method that accepts a name from the user and then returns it:
+
 # const getName = () => {
 #   let name = prompt("what is your name?");
 #   return name;
@@ -8,9 +12,207 @@
 def getName():
     user_input = input("what is your name?")
     print("Hello, " + user_input + "!")
-    
+
 getName()
 
+
+
+
+
+
+
+
+
 # ================================ Problem 2 ================================
+# Write a method that reverses a string:
+
+# const reverseIt = () => {
+#   let string = "a man, a plan, a canal, frenemies!";
+
+#   let reverse = "";
+
+#   for (let i=0; i < string.length; i++) {
+#     reverse += string[string.length - (i+1)];
+#   };
+
+#   alert(reverse);
+# };
+
+
+
+
+
+
+
+
+
+# ================================ Problem 3 ================================
+# Write a method that swaps the values of two variables around. Here’s the javascript:
+
+# const swapEm = () => {
+#   let a = 10;
+#   let b = 30;
+#   let temp;
+
+#   temp = b;
+#   b = a;
+#   a = temp;
+
+#   alert("a is now " + a + ", and b is now " + b);
+# };
+
+
+
+
+
+
+
+
+
+# ================================ Problem 4 ================================
+# Write a method that multiplies all numbers in a given array/list and returns the final product. Here’s the javascript:
+
+# const multiplyArray = (ary) => {
+#   if (ary.length == 0) { return 1; };
+
+#   let total = 1;
+#   // let total = ary[0];
+
+#   for (let i=0; i < ary.length; i++) {
+#     total = total * ary[i];
+#   };
+
+#   return total;
+# };
+
+
+
+
+
+
+
+
+
+# ================================ Problem 5 ================================
+# Write a method that takes a number argument and returns “fizz” if the number is divisible by three, “buzz” if the number is divisible by five, and “fizzbuzz” if it’s divisible by both. Here’s the javascript:
+
+# const fizzbuzzer = (x) => {
+#   if( x%(3*5) == 0 ) {
+#     return 'fizzbuzz'
+#   } else if( x%3 == 0 ) {
+#     return 'fizz'
+#   } else if ( x%5 == 0 ) {
+#     return 'buzz'
+#   } else {
+#     return 'archer'
+#   }
+# }
+
+
+
+
+
+
+
+
+# ================================ Problem 6 ================================
+# Write a method that finds the fibonacci number at the nth position and returns it. Here is the javascript:
+
+# const nthFibonacciNumber = () => {
+#   let fibs = [1, 1];
+#   let num = prompt("which fibonacci number do you want?");
+
+#   while (fibs.length < parseInt(num)) {
+#     let length = fibs.length;
+#     let nextFib = fibs[length - 2] + fibs[length - 1];
+#     fibs.push(nextFib);
+#   }
+
+#   alert(fibs[fibs.length - 1] + " is the fibonacci number at position " + num);
+# };
+
+
+
+
+
+
+
+
+
+# ================================ Problem 7 ================================
+# Write a method that searches through an array/list for a value and returns true or false depending on whether or not the value is present in the array/list. Here is the javascript:
+
+# const searchArray = (array, value) => {
+#   for(let i = 0; i < array.length-1; i++) {
+#     if(array[i] == value) {
+#       return true;
+#       break;
+#     }
+#   }
+#   return -1;
+# };
+
+
+
+
+
+
+
+
+
+
+# ================================ Problem 8 ================================
+# Write a method that checks whether or not a string is a palindrome. Here is the javascript:
+
+# const isPalindrome = (str) => {
+#   for(let i = 0; i < str.length/2; i++){
+#     if(str[i] != str[str.length-i-1]){
+#       return false;
+#       break;
+#     }
+#   }
+#   return true;
+# };
+
+
+
+
+
+
+
+
+
+
+# ================================ Problem 9 ================================
+# Write a method that checks whether or not an array/list has any duplicates. Here is the javascript:
+
+# const hasDupes = (arr) => {
+#   let obj = {};
+#   for (i = 0; i < arr.length; i++) {
+#     if(obj[arr[i]]) {
+#       return true;
+#     }
+#     else {
+#       obj[arr[i]] = true;
+#     }
+#   }
+#   return false;
+# };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
